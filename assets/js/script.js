05c4c36749cbe12435a3390ac873176b0a2934ee
Becca Wyland <rwyland@uwalumni.com>
2021-03-24 21:26:21 -0500

    create array of objects for list of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,34 @@ var startBtn = document.querySelector("#start");
 
 // Perform quiz
 function runQuiz() {
-    
+    // display first of multiple questions within in array of objects
+    var questionList = [
+        {
+            question: "question1",
+            choices: ["choice1.1", "choice1.2", "choice1.3", "choice1.4"],
+            answer: "answer1"
+        },
+        {
+            question: "question2",
+            choices: ["choice2.1", "choice2.2", "choice2.3", "choice2.4"],
+            answer: "answer2"
+        },
+        {
+            question: "question3",
+            choices: ["choice3.1", "choice3.2", "choice3.3", "choice3.4"],
+            answer: "answer3"
+        },
+        {
+            question: "question4",
+            choices: ["choice4.1", "choice4.2", "choice4.3", "choice4.4"],
+            answer: "answer4"
+        },
+        {
+            question: "question5",
+            choices: ["choice5.1", "choice5.2", "choice5.3", "choice5.4"],
+            answer: "answer5"
+        }
+    ];
 }
 
 // Add event listener to start button
