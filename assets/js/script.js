82f1221868a2c94bbb680c6b224bae382ec47ba6
Becca Wyland <rwyland@uwalumni.com>
2021-03-24 21:00:38 -0500

    set up js and create an event listener for start button

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Get references to the #start element
+var startBtn = document.querySelector("#start");
+
+// Perform quiz
+function runQuiz() {
+    
+}
+
+// Add event listener to start button
+startBtn.addEventListener("click", runQuiz);
